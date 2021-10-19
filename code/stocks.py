727a52dd5623e344f664b33fba79b539d1a2154d
Aditya Jain <thisisadityajain@gmail.com>
2021-10-19 22:57:09 +0530

    completed stock tab

--- code/stocks.py
+++ code/stocks.py
@@ -1,7 +1,4 @@
-from logging import disable
 import plotly.express as px
-import dash
-from datetime import date
 from dash import html
 from dash import dcc
 import dash_daq as daq
@@ -92,7 +89,7 @@ stocks_layout = html.Div(
                         ),
 
                     ],
-                    width=7),
+                    width=8),
 
 
                 dbc.Col(
@@ -110,7 +107,7 @@ stocks_layout = html.Div(
                                 "red": [0, 1.33], "yellow":[1.33, 2.66], "green":[2.66, 4]}},
                         ),
                     ],
-                    width=5),
+                    width=4),
             ]
         ),
         # radar chart row
@@ -120,14 +117,14 @@ stocks_layout = html.Div(
                     [
                         dcc.Graph(id="radar-stock", figure={})
                     ],
-                    width=6),
+                    width=7),
 
 
                 dbc.Col(
                     [
                         dcc.Graph(id="indicator-stock", figure={})
                     ],
-                    width=6),
+                    width=5),
             ]
         ),
     ]
@@ -183,13 +180,10 @@ def on_switch_candle_change(isOn, stocks):
 
 @app.callback(
     Output(component_id="graph-stock", component_property="figure"),
-    # Output(component_id="gauge-stock", component_property="value"),
-    # Output(component_id="radar-stock", component_property="figure"),
-    # Output(component_id="indicator-stock",
-    #        component_property="figure"),
-    # Output(component_id="switch-candle", component_property="on"),
-    # Output(component_id="switch-percent", component_property="on"),
-    # Output(component_id="slider-moving-average", component_property="value"),
+    Output(component_id="gauge-stock", component_property="value"),
+    Output(component_id="radar-stock", component_property="figure"),
+    Output(component_id="indicator-stock",
+           component_property="figure"),
     Input(component_id="btn-stock-run",
           component_property="n_clicks"),
     State(component_id="switch-candle", component_property="on"),
@@ -206,23 +200,86 @@ def on_switch_candle_change(isOn, stocks):
     prevent_initial_call=True
 )
 def on_apply_changes(n_clicks, candle_on, percent_on, moving_average_value, industry, stocks, start_date, end_date):
-
+    start_datetime = pd.to_datetime(start_date)
+    end_datetime = pd.to_datetime(end_date)
+    # Stock Graph
     selected_stock_df = all_stock_df.loc[all_stock_df["Symbol"].isin(stocks)]
-    selected_stock_df = selected_stock_df.loc[(selected_stock_df["Date"] >= pd.to_datetime(
-        start_date)) & (selected_stock_df["Date"] <= pd.to_datetime(end_date))]
+    selected_stock_df = selected_stock_df.loc[(selected_stock_df["Date"] >= start_datetime) & (
+        selected_stock_df["Date"] <= end_datetime)]
 
-    candle_stock_df = selected_stock_df.loc[selected_stock_df["Symbol"] == stocks[0]]
     if candle_on:
+        candle_stock_df = selected_stock_df.loc[selected_stock_df["Symbol"] == stocks[0]]
         stock_fig = go.Figure(data=[go.Candlestick(x=candle_stock_df['Date'],
                                                    open=candle_stock_df['Open'], high=candle_stock_df['High'],
                                                    low=candle_stock_df['Low'], close=candle_stock_df['Close']),
                                     ], layout=go.Layout(
-            title=go.layout.Title(text=stocks[0])
+            title=go.layout.Title(text=stocks[0]), shapes=[dict(
+                x0=date, x1=date, y0=0, y1=1, xref='x', yref='paper',
+                line_width=2) for date in candle_stock_df.drop_duplicates('Prime minister', keep="first")["Date"]],
+            annotations=[dict(
+                x=data[1]["Date"], y=0.05, xref='x', yref='paper',
+                showarrow=False, xanchor='left', text=data[1]["Prime minister"]) for data in candle_stock_df.drop_duplicates('Prime minister', keep="first").iterrows()]
         ))
     else:
         selected_stock_df["Rolling mean"] = selected_stock_df["Percent change" if percent_on else "Close"].rolling(
             window=moving_average_value).mean()
         stock_fig = px.line(selected_stock_df, title=industry, x="Date", y="Rolling mean", color="Symbol",
                             line_group="Symbol", hover_name="Symbol", line_shape="spline", render_mode="svg")
+        for data in selected_stock_df.drop_duplicates('Prime minister', keep="first").iterrows():
+            stock_fig.add_annotation(x=data[1]["Date"], y=0.05,
+                                     text=data[1]["Prime minister"],)
     stock_fig.update_xaxes(rangeslider_visible=True)
-    return stock_fig
+
+    # Gauge chart
+    perc_change_start = selected_stock_df.loc[selected_stock_df["Date"] ==
+                                              start_datetime, "Percent change"].mean()
+    perc_change_end = selected_stock_df.loc[selected_stock_df["Date"] ==
+                                            end_datetime, "Percent change"].mean()
+    gauge_value = 3 if perc_change_end > perc_change_start else 1
+    # Radar chart
+    radar_fig = go.Figure()
+    radar_categories = selected_stock_df["Finance minister"].unique()
+    for stock in stocks:
+        radar_fig.add_trace(go.Scatterpolar(
+            r=selected_stock_df.loc[selected_stock_df["Symbol"]
+                                    == stock].groupby(by="Finance minister").mean()["Percent change"],
+            theta=radar_categories,
+            fill='toself',
+            name=stock
+        ))
+    # indicators
+
+    data_dict = {"Symbol": [], "Initial close": [], "Final close": []}
+
+    for stock in stocks:
+        if stock not in selected_stock_df["Symbol"].unique():
+            continue
+        data_dict["Symbol"].append(stock)
+        stock_df = selected_stock_df.loc[selected_stock_df["Symbol"] == stock]
+        data_dict["Initial close"].append(stock_df.iloc[0]["Close"])
+        data_dict["Final close"].append(stock_df.iloc[-1]["Close"])
+    data_df = pd.DataFrame.from_dict(data_dict)
+    data_df["Diff"] = data_df["Final close"] - data_df["Initial close"]
+    data_df = data_df.sort_values(by=["Diff"])
+
+    indicator_fig = go.Figure()
+    indicator_fig.add_trace(go.Indicator(
+        mode="number+delta",
+        value=data_df.iloc[-1]["Final close"],
+        title={"text": "{}<br><span style='font-size:0.8em;color:gray'>Best performing Stock</span>".format(
+            data_df.iloc[-1]["Symbol"])},
+        delta={'reference': data_df.iloc[-1]
+               ["Initial close"], 'relative': True},
+        domain={'x': [0.5, 1], 'y': [0, 0.5]},))
+
+    indicator_fig.add_trace(go.Indicator(
+        mode="number+delta",
+        value=data_df.iloc[0]["Final close"],
+        title={"text": "{}<br><span style='font-size:0.8em;color:gray'>Worst performing Stock</span>".format(
+            data_df.iloc[0]["Symbol"])},
+
+        delta={'reference': data_df.iloc[0]
+               ["Initial close"], 'relative': True},
+        domain={'x': [0.5, 1], 'y': [0.5, 1]},))
+
+    return stock_fig, gauge_value, radar_fig, indicator_fig
